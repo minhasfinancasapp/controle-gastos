41bd083cb708c861cb54f733f251816bff0aa82d
plasmafelipe <plasmafelipe@gmail.com>
2026-05-05 23:31:08 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,17 @@
-const CACHE_NAME = "controle-gastos-supabase-v1";
-const FILES = ["./", "./index.html", "./manifest.json", "./supabase-config.js"];
+const CACHE_NAME = "controle-gastos-cloud-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon.svg", "./supabase-config.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
