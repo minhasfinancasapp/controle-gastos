deb69065f90be47b63ed65bf4e9e19d6c7419b81
minhasfinancasapp <plasmafelipe@gmail.com>
2026-05-11 22:38:11 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,7 @@
-const SUPABASE_URL = "https://qdglqfjhncjmkfoowqxu.supabase.co";
+// COLE AQUI OS DADOS DO SEU SUPABASE
+// Exemplo:
+// const SUPABASE_URL = "https://seuprojeto.supabase.co";
+// const SUPABASE_ANON_KEY = "sua_publishable_key";
 
-const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
+const SUPABASE_URL = "COLE_AQUI_A_URL_DO_PROJETO";
+const SUPABASE_ANON_KEY = "COLE_AQUI_A_PUBLISHABLE_KEY";
